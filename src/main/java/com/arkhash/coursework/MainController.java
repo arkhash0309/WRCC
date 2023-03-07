838ded8095e34d6ec2003703b37ce2b4ff15e9c6
Arkhash <arkhash.0309@gmail.com>
2023-03-07 19:33:33 +0530

    Completed ADD function.

--- src/main/java/com/arkhash/coursework/MainController.java
+++ src/main/java/com/arkhash/coursework/MainController.java
@@ -2,6 +2,7 @@ package com.arkhash.coursework;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,61 +13,72 @@ public class MainController {
     @FXML
     private TextField nameField, ageField, teamField, modelField, pointsField, idField;
 
-
+    private boolean isFieldInvalid(TextField field) {
+        if (field == nameField) {
+            return Utils.hasSpecialCharacters(field.getText());
+        } else if (field == ageField) {
+            int age = Utils.getNumber(field.getText());
+            return age == -1;
+        } else if (field == teamField) {
+            return Utils.hasSpecialCharacters(field.getText());
+        } else if (field == modelField) {
+            return Utils.isEmpty(field.getText());
+        } else if (field == pointsField) {
+            int points = Utils.getNumber(field.getText());
+            return points == -1;
+        } else {
+            return Utils.isEmpty(field.getText());
+        }
+    }
 
     @FXML
-    protected void onNameChanged() {
-        System.out.println("hello");
+    protected void onTextChanged(KeyEvent e) {
+        TextField field = (TextField) e.getSource();
+        field.getStyleClass().remove("invalid-field");
+        field.getStyleClass().remove("valid-field");
+        if (isFieldInvalid(field)) {
+            field.getStyleClass().add("invalid-field");
+        } else {
+            field.getStyleClass().add("valid-field");
+        }
     }
 
     @FXML
     protected void onSubmitButtonClick() throws IOException {
         // performs the respective check
-        if (Utils.hasSpecialCharacters(nameField.getText())) {
+        if (isFieldInvalid(nameField)) {
             Utils.errorDialog("Please check the name again.");
             return;
         }
 
         int age = Utils.getNumber(ageField.getText());
-        if (age == -1) {
+        if (isFieldInvalid(ageField)) {
             Utils.errorDialog("Please check the age again.");
             return;
         }
 
-        if (ageField.getText() == null) {
-            Utils.errorDialog("Please check the age again.");
-            return;
-        }
-
-        if (Utils.hasSpecialCharacters(teamField.getText())) {
+        if (isFieldInvalid(teamField)) {
             Utils.errorDialog("Please check the team name again.");
             return;
         }
 
-        if (Utils.hasSpaces(modelField.getText())) {
+        if (isFieldInvalid(modelField)) {
             Utils.errorDialog("Please check the model again.");
             return;
         }
 
-        if (Utils.hasSpaces(idField.getText())) {
+        if (isFieldInvalid(idField)) {
             Utils.errorDialog("Please check the ID again.");
             return;
         }
 
         int initialPoints = Utils.getNumber(pointsField.getText());
-        if (initialPoints == -1) {
-            Utils.errorDialog("Please check the championship points again.");
-            return;
-        }
-
-        if (pointsField.getText() == null) {
+        if (isFieldInvalid(pointsField)) {
             Utils.errorDialog("Please check the championship points again.");
             return;
         }
 
         Driver driverStore = new Driver(nameField.getText(), age, teamField.getText(), modelField.getText(), initialPoints, idField.getText());
         drivers.add(driverStore);
-
-
     }
 }
